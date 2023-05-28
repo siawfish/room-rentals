c88e99cd355cf3236c4a97361bc36297c1d9fe24
siawfish <mcamanor@gmail.com>
2023-05-28 04:41:39 +0000

    Login image fixes

--- app/api/auth/signIn/page.tsx
+++ app/api/auth/signIn/page.tsx
@@ -3,6 +3,7 @@ import LoginForm from '../../../components/LoginForm';
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../../../../pages/api/auth/[...nextauth]"
 import { redirect } from 'next/navigation';
+import logoImage from '/assets/images/logo.jpeg';
 
 export const dynamic = 'force-dynamic';
 
@@ -17,7 +18,7 @@ export default async function IndexPage() {
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <Image className="mx-auto h-10 w-auto rounded-full" width={100} height={100} src={`${process.env.NEXTAUTH_URL}/assets/images/logo.jpeg`} alt="Your Company" />
+          <Image className="mx-auto h-10 w-auto rounded-full" width={100} height={100} src={logoImage} alt="Your Company" />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
         </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
