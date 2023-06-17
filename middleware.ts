ca2280bf299aa61c4ec40c96bb91a3daf2d68d0b
siawfish <mcamanor@gmail.com>
2023-06-17 17:41:30 +0000

    More updates

--- middleware.ts
+++ middleware.ts
@@ -11,6 +11,12 @@ export default withAuth(
       // @ts-ignore
       return NextResponse.redirect(new URL(`/motels/${req.nextauth.token?.data?.user?.motel_id}`, req.url));
     }
+    // if the next path after /motals is not a number and is not equal to the user motel_id and the user is not superadmin
+    // @ts-ignore
+    if(req.nextUrl.pathname !== '/motels' && isNaN(req.nextUrl.pathname.split('/')[2]) && req.nextUrl.pathname.split('/')[2] !== req.nextauth.token?.data?.user?.motel_id && req.nextauth.token?.data?.user?.role !== "superadmin"){
+      // @ts-ignore
+      return NextResponse.redirect(new URL(`/motels/${req.nextauth.token?.data?.user?.motel_id}`, req.url));
+    }
   },
   {
     callbacks: {
